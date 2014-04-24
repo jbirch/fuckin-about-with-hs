40a5627393d21e38b595851e1e22e25af468bbd1
Jason Birch <jbirch@jbirch.net>
2014-04-25 03:00:47 +1000

    Turn application into composition, remove braces.

--- src/Main.hs
+++ src/Main.hs
@@ -5,15 +5,15 @@ import Data.Foldable (toList)
 import Data.Sequence
 
 qs :: Ord a => [a] -> [a]
-qs xs = toList $ qsSeq $ fromList xs
+qs xs = toList . qsSeq . fromList $ xs
 
 
 qsSeq :: Ord a => Seq a -> Seq a
 qsSeq (viewl -> EmptyL)  = empty
-qsSeq (viewl -> x :< xs) = (qsSeq less |> x ) >< qsSeq more
+qsSeq (viewl -> x :< xs) = qsSeq less >< x <| qsSeq more
     where
         less = filter (<  x) xs
         more = filter (>= x) xs
 
 main :: IO ()
-main = print $ qs [100, 99..0]
+main = print . qs $  [100, 99..0]
