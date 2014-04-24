2d97763205ace6813fafe308b2c5f7e7d6cb2e79
Jason Birch <jbirch@jbirch.net>
2014-04-24 22:40:53 +1000

    Add a crappy quicksort algo

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,19 @@
+{-# LANGUAGE ViewPatterns #-}
+
+import Prelude hiding (filter)
+import Data.Foldable (toList)
+import Data.Sequence
+
+qs :: Ord a => [a] -> [a]
+qs xs = toList $ qsSeq $ fromList xs
+
+
+qsSeq :: Ord a => Seq a -> Seq a
+qsSeq (viewl -> EmptyL)  = empty
+qsSeq (viewl -> x :< xs) = (qsSeq less |> x ) >< qsSeq more
+    where
+        less = filter (<  x) xs
+        more = filter (>= x) xs
+
 main :: IO ()
-main = putStrLn "Shitty main."
+main = print $ qs [100, 99..0]
