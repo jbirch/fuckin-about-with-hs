5f6bbcb470e8cddfc1b04c206a205379f25fd24b
Jason Birch <jbirch@jbirch.net>
2014-05-04 01:50:29 +1000

    Add json handling, type param explicitly

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,17 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
-import           Data.Monoid (mconcat)
+import           Data.Monoid      (mconcat)
 --Import           QS         (qs)
+import           Data.Aeson.Types
 import           Web.Scotty
+
 
 main :: IO ()
 main = scotty 3000 $ get "/" $ do
-         fooParam <- param "foo"
-         html $ mconcat ["Derp", fooParam]
+         (fooParam :: String) <- param "foo"
+         json $ toJSON ["Derp ", fooParam]
 
 -- mconcat . show . (qs :: [Integer] -> [Integer]) $  [100, 99..0]
