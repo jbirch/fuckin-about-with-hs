570286b649f7caf2a17e4dee0b77c53849f79da4
Jason Birch <jbirch@jbirch.net>
2014-05-04 01:35:19 +1000

    Add a shitty required parameter

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,13 @@
 
 module Main (main) where
 
---import           Data.Monoid (mconcat)
---import           QS         (qs)
+import           Data.Monoid (mconcat)
+--Import           QS         (qs)
 import           Web.Scotty
 
 main :: IO ()
-main = scotty 3000 $ get "/" $ html "Derp"
+main = scotty 3000 $ get "/" $ do
+         fooParam <- param "foo"
+         html $ mconcat ["Derp", fooParam]
 
 -- mconcat . show . (qs :: [Integer] -> [Integer]) $  [100, 99..0]
