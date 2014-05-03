44b292b35bc018e6c3decc11178dff8aaaec30fb
Jason Birch <jbirch@jbirch.net>
2014-05-04 01:53:48 +1000

    Return the quicksorted list as json now.

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,10 @@
 
 module Main (main) where
 
-import           Data.Monoid      (mconcat)
---Import           QS         (qs)
 import           Data.Aeson.Types
+import           QS               (qs)
 import           Web.Scotty
 
 
 main :: IO ()
-main = scotty 3000 $ get "/" $ do
-         (fooParam :: String) <- param "foo"
-         json $ toJSON ["Derp ", fooParam]
-
--- mconcat . show . (qs :: [Integer] -> [Integer]) $  [100, 99..0]
+main = scotty 3000 $ get "/" $ json . toJSON . (qs :: [Integer] -> [Integer]) $  [100, 99..0]
